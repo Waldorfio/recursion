a71ac38da7801a7637835b308f3acda4de633e3d
Wally <107082066+Waldorfio@users.noreply.github.com>
2022-09-12 15:12:26 +1000

    Add working merge function

--- Assignment 2/code.js
+++ Assignment 2/code.js
@@ -1,35 +1,37 @@
 console.clear();
 console.log('----');
 
-
-newArr = [];
 function mergeSort(arr) {
-
-    // Base Case
-    if (arr[0] > arr[-1]) {
-        console.log('done: '+newArr);
-        return
-    }
-    
-    mid = Math.ceil(arr.length/2);
-
-    mergeSort(arr.slice(0, mid));
-    mergeSort(arr.slice(mid+1, arr.length));
-
-    // Merge
-
-
-
-
+  // Base Case - to return sorted list (i.e. single numbers)
+  if (arr.length <= 1) {
+    return arr
+  }
+  // Finding Mid-point
+  let mid = arr.length / 2;
+  // Seperating RHS and LHS of array
+  let LHS = arr.splice(0, mid);
+  let RHS = arr;          // RHS is just equal to whatever was leftover in the splicing
+  return merge(mergeSort(LHS), mergeSort(RHS));
 }
 
-// Pseudocode
-// if left > right 
-// return
-// mid = (left+right)/2
-// mergeSort(arr, left, mid)
-// mergeSort(arr, mid+1, right)
-// merge(arr, left, mid, right)
-// end
+function merge(LHS, RHS) {
+  let sorted = []
 
-mergeSort([8,9,3,8,7,5]);
+  while (LHS.length > 0 && RHS.length > 0) {
+      if (LHS[0] < RHS[0]) {
+        sorted.push(LHS[0]);
+        LHS.shift();
+      } else {    // Else add the other value to the array
+        sorted.push(RHS[0]);
+        RHS.shift();
+      }
+  }
+  // Adding leftover elements to the merged array
+  if (LHS > 0) {sorted.push(LHS)};
+  if (RHS > 0) {sorted.push(RHS)};
+  return sorted
+}
+
+inp = [1,8,2,6,7,3,5,4,9];
+a = mergeSort(inp);
+console.log('ans: '+a)
